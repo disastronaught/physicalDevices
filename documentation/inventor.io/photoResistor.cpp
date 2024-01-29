38362e0ac290d26f903881e29008def876fc6b60
Odin <139891768+disastronaught@users.noreply.github.com>
2024-01-28 21:30:56 -0500

    Update photoResistor.cpp

--- documentation/inventor.io/photoResistor.cpp
+++ documentation/inventor.io/photoResistor.cpp
@@ -1,8 +1,8 @@
 #include "Arduino.h"
 
 const byte PHOTORESISTOR_PIN = A0;
-const unasigned int MIN_DELAY = 50;
-const unasigned int MAX_DELAY = 500;
+const unsigned int MIN_DELAY = 50;
+const unsigned int MAX_DELAY = 500;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -11,12 +11,12 @@ void setup() {
 }
 
 void loop() {
-  unasigned int light_value = analogRead(PHOTORESISTOR_PIN);
+  unsigned int light_value = analogRead(PHOTORESISTOR_PIN);
   Serial.print("Light value: ");
   Serial.print(light_value);
 
-  static unasigned int darkest_light = light_value;
-  static unasigned int brightest_light = light_value;
+  static unsigned int darkest_light = light_value;
+  static unsigned int brightest_light = light_value;
 
   if (light_value < darkest_light) {
     darkest_light = light_value;
@@ -25,7 +25,7 @@ void loop() {
     brightest_light = light_value;
   }
 
-  unasigned int delay_value = map(light_value, darkest_light, brightest_light, MAX_DELAY, MIN_DELAY);
+  unsigned int delay_value = map(light_value, darkest_light, brightest_light, MAX_DELAY, MIN_DELAY);
   Serial.print(", Delay value: ");
   Serial.println(delay_value);
 
